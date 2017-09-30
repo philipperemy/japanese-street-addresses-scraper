a64d1d3a1500a424c27ba215210916864b9f3dbb
philipperemy <premy@reactive.co.jp>
2017-09-30 14:34:56 +0900

    we take everything!

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import json
 import logging
 from glob import glob
+from random import shuffle
 
 from new_scrape import change_ip
 from new_scrape import run_scrape
+
+
+def run_random():
+    files = glob('regions/*.json')
+    all_urls = []
+    for json_file in files:
+        with open(json_file, 'rb') as j:
+            data = json.load(j)
+            for region_name, region_data in data.items():
+                print(region_name)
+                urls = region_data['sub_region']['level_2']
+                urls = sorted(list(set(urls)))  # make sure no redundancy.
+                all_urls.extend(urls)
+
+    # shuffle !!
+    shuffle(all_urls)
+    for i, url in enumerate(all_urls):
+        if i % 10 == 0:
+            logging.info('IP SWITCHING.')
+            change_ip()  # we do not want to fire all our IPs. So lets switch from time to time.
+        logging.info('({1}/{2}) MAIN - REQUESTING {0}'.format(url, i, len(all_urls)))
+        run_scrape(url)
 
 
 def run():
@@ -24,4 +47,4 @@ def run():
 
 
 if __name__ == '__main__':
-    run()
+    run_random()
