e4d3d6b87e9fb20154909b350799fa3fd5810eb7
philipperemy <premy@reactive.co.jp>
2017-09-30 17:56:26 +0900

    we take everything!

---
+++ main.sh
@@ -0,0 +1,5 @@
+while true; do
+    nohup python3 -u main.py &
+    sleep 3600
+    ps aux | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} python | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} -v "grep python" | awk '{print $2}' | xargs kill -9
+done
