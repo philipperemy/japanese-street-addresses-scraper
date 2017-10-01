92eebf71a40792c50fbf23b6f0a3005a26ac5313
philipperemy <premy@reactive.co.jp>
2017-10-01 10:05:09 +0900

    we take everything!

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 while true; do
+    echo Starting the program
     nohup python3.6 -u main.py &
+    echo Going to sleep for one hour now
     sleep 3600
+    echo I am awake now lets go
     ps aux | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} python | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} -v "grep python" | awk '{print $2}' | xargs kill -9
+    echo Program should be dead by now
 done
