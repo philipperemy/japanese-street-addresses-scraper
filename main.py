bb784d63197007e9157dfb0bb0408be2771b1efa
philipperemy <premy@reactive.co.jp>
2017-10-15 21:56:19 +0900

    cleansing 1

--- main.py
+++ main.py
@@ -36,22 +36,5 @@ def run_random():
             query_iterations += 1
 
 
-# def run():
-#     files = glob('regions/*.json')
-#     for json_file in files:
-#         with open(json_file, 'rb') as j:
-#             data = json.load(j)
-#             for region_name, region_data in data.items():
-#                 print(region_name)
-#                 urls = region_data['sub_region']['level_2']
-#                 urls = sorted(list(set(urls)))  # make sure no redundancy.
-#                 for i, url in enumerate(urls):
-#                     if i % 10 == 0:
-#                         logging.info('IP SWITCHING.')
-#                         change_ip()  # we do not want to fire all our IPs. So lets switch from time to time.
-#                     logging.info('({1}/{2}) MAIN - REQUESTING {0}'.format(url, i, len(urls)))
-#                     run_scrape(url)
-
-
 if __name__ == '__main__':
     run_random()
--- run_brute_force_urls_mt.sh
+++
@@ -1,5 +0,0 @@
-ps aux | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} python | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} -v "grep python" | awk '{print $2}' | xargs kill -9
-rm -rf /tmp/brute.out
-nohup python3.6 -u brute_force_all_endpoints_mt.py > /tmp/brute.out &
-sleep 2
-tail -f /tmp/brute.out
